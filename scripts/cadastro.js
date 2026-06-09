91e1083e0387778d8606bc36425d21dd6f266920
aksf0 <aksfornai@gmail.com>
2026-06-08 22:42:13 -0300

    atualizacao

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,16 +1,41 @@
-let logado = false;
+let logado = localStorage.getItem("logado") === "true";
+
+
+if (logado && document.querySelector(".ladoDireito")){
+    document.querySelector(".login").remove();
+    document.querySelector(".cadastro").remove();
+
+    let nomeUsuario = document.createElement("p");
+    document.querySelector(".ladoDireito").appendChild(nomeUsuario);
+    const dadosUsuario = JSON.parse(localStorage.getItem("cadastroUsuario"))
+    nomeUsuario.innerText = dadosUsuario[0];
+    nomeUsuario.style.fontSize = "30px"
+}
 
 if (document.querySelector(".cadastroForm")) {
-    document.querySelector(".cadastroForm").addEventListener("submit", salvarDados)
+    document.querySelector(".cadastroForm").addEventListener("submit", function(e) {
 
-    function salvarDados() {
         const nomeUser = document.getElementById("userNome").value;
         const emailUser = document.getElementById("userEmail").value;
         const senhaUser = document.getElementById("userSenha").value;
         const dadosUser = [nomeUser, emailUser, senhaUser];
 
-        localStorage.setItem("cadastroUsuario", JSON.stringify(dadosUser));
-    };
+        let dadosVerificados = 0;
+        dadosUser.forEach(element => {
+            //consertar isso
+            if (element.lenght >= 3){
+                dadosVerificados ++;
+            }
+        });
+        if (dadosVerificados == 3){
+            localStorage.setItem("cadastroUsuario", JSON.stringify(dadosUser));
+            document.getElementById("cadastroRetorno").innerText = "";
+        }else{
+            e.preventDefault();
+            document.getElementById("cadastroRetorno").innerText = "Cada campo deve conter pelo menos 3 caracteres";
+            document.getElementById("cadastroRetorno").style.color = "red";
+        }
+    })
 };
 
 if (document.querySelector(".loginForm")) {
@@ -24,6 +49,7 @@ if (document.querySelector(".loginForm")) {
 
         if (emailUser == dados[1] && senhaUser == dados[2]) {
             logado = true;
+            localStorage.setItem("logado", logado);
             retorno.innerText = "";
             btnTrocarPagina.innerHTML = "<p>Login realizado<br>Clique aqui para voltar</br></p>";
             btnTrocarPagina.style.backgroundColor = "green";
